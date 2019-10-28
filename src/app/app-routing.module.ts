7e2a88eea5d63b2c09fc15d03938d1dba1db9a9f
med melek <medmelek007@gmail.com>
2019-10-28 03:19:32 -1200

    modify splatch screen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,10 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
+  { path: 'splash-screen', loadChildren: './splash-screen/splash-screen.module#SplashScreenPageModule' },
+  { path: 'feed', loadChildren: './feed/feed.module#FeedPageModule' },
 ];
 
 @NgModule({
